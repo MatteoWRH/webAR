bd5f7c483c9c146c4f3f1a9a8231ceec62c14b48
MatteoWRH <matteo@werhappy.com>
2019-07-23 12:34:09 +0200

    asdf

--- 1proto.js
+++ 1proto.js
@@ -19,12 +19,12 @@ AFRAME.registerComponent('registerevents', {
 			var marker = this.el;
 			marker.addEventListener('markerFound', function() {
 				var markerId = marker.id;
-				console.log('markerFound', markerId);
+				alert('markerFound', markerId);
 				// TODO: Add your own code here to react to the marker being found.
 			});
 			marker.addEventListener('markerLost', function() {
 				var markerId = marker.id;
-				console.log('markerLost', markerId);
+				alert.log('markerLost', markerId);
 				// TODO: Add your own code here to react to the marker being lost.
 			});
 		}
